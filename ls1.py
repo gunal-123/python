473ee61706d3c3bee39e413c0e2106f7ff164158
Gunal <103403219+gunal-123@users.noreply.github.com>
2022-10-29 20:45:06 +0530

    Update ls1.py

--- ls1.py
+++ ls1.py
@@ -1,1 +1,11 @@
-print('hello world')
+def armstrong(n):
+  rev = 0
+  a = 0
+  while n > 0:
+    rev = (n % 10)
+    a = a + rev**3
+    n = n // 10
+  return a,n
+n = int(input())
+if armstrong(n) == n: print('armstrong')
+else: print('not')
